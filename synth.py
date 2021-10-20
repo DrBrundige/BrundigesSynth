1629e58cf289e2f28d9ba235a62386b279ddf9b4
Dr. Brundige <brundigejones@gmail.com>
2021-10-20 14:13:01 -0500

    Clicker, Desampler, Inverted Beeper
    Adds the Clicker, Desampler, and Inverted Beeper classes

--- synth.py
+++ synth.py
@@ -140,6 +140,32 @@ class Beeper(Oscillator):
 			self.wave.append(sample)
 
 
+class InvertedBeeper(Beeper):
+	k = 1
+
+	def __next__(self):
+		# print(self.i < self.end)
+		if self.i < self.num_samples:
+			r = self.j * self.period
+			self.i += 1
+			if r >= math.pi:
+				r = 0.0
+				self.j = 1
+				self.k = self.k * -1
+			else:
+				self.j += 1
+
+			# r += self.phase
+			sample = int((1 - math.sin(r)) * self.amplitude)
+			for filter in self.all_filters:
+				sample = filter.filter(sample)
+			# Clip - if r is greater than bit depth, return bit depth
+			# print(math.sin(r) + 1)
+			return sample
+		else:
+			raise StopIteration
+
+
 class SawtoothBuzzer(Beeper):
 	# Synthesizer environment. Standardizes sample rate and bit depth
 
@@ -226,6 +252,51 @@ class HarmonicBeeper(Beeper):
 			raise StopIteration
 
 
+class Clicker(Beeper):
+	# Number of clicks per minute
+	click_frequency = 0
+	# Frequency of wave this clicker will produce while it is clicking
+	click_waves = 1
+	waves_remaining = 1
+
+	def __init__(self, synth, duration, bpm, frequency=110, click_waves=1, amplitude=1.0):
+		super().__init__(synth, duration, frequency, amplitude)
+		# Number of samples between clicks
+		self.click_frequency = int((self.Synth.sample_rate / (bpm / 60)))
+
+		# Number of waves in each click
+		self.click_waves = click_waves
+		self.waves_remaining = click_waves
+
+	def __next__(self):
+		# print(self.i < self.end)
+		if self.i < self.num_samples:
+			sample = 0
+
+			if self.i % self.click_frequency == 0:
+				self.waves_remaining = self.click_waves
+			self.i += 1
+
+			if self.waves_remaining > 0:
+				r = self.j * self.period
+				if r >= math.pi * 2:
+					self.j = 1
+					self.waves_remaining -= 1
+				else:
+					self.j += 1
+					sample = int(math.sin(r) * self.amplitude)
+					for filter in self.all_filters:
+						sample = filter.filter(sample)
+
+			return sample
+		else:
+			raise StopIteration
+
+	def play(self):
+		for sample in self:
+			self.wave.append(sample)
+
+
 class NoiseMaker(Oscillator):
 	# Synthesizer environment. Standardizes sample rate and bit depth
 	Synth = BrundigesSynth()
@@ -260,18 +331,48 @@ class NoiseMaker(Oscillator):
 			self.wave.append(sample)
 
 
+# Rounds down samples, creating a blockier wave
 class LofiModulus(Filter):
+	# Integer to which samples will be rounded.
+	# Modulo 1 rounds down to whole number. Mod 2 rounds to even number, etc.
 	modulo = 1
 
 	def __init__(self, modulo):
 		self.modulo = modulo
 
 	def filter(self, sample):
-		# print("Filtering Wave")
+		# Divides sample by modulo, rounds it down, then multiplies it back
+		# There is a way to do this that actually uses the modulus,
+		#   but this is who it is done in Excel, so it's what I know
 		sliced_sample = sample / self.modulo
 		sliced_sample = math.floor(sliced_sample)
 		sliced_sample = sliced_sample * self.modulo
 		return sliced_sample
+
+
+# Copies samples over each other, creating a blockier wave.
+class LofiDesampler(Filter):
+	# Number of samples to hold
+	sample_copies = 1
+	# Counter
+	i = 0
+	# Saves value of the last held sample
+	last_sample = 0
+
+	def __init__(self, sample_copies):
+		assert sample_copies > 0, "Errant input! sample_copies must be greater than 0!"
+		self.sample_copies = sample_copies
+		self.i = 0
+		self.last_sample = 0
+
+	def filter(self, sample):
+		if self.i > 0:
+			self.i -= 1
+		else:
+			self.i = self.sample_copies
+			self.last_sample = sample
+
+		return self.last_sample
 
 
 class Square(Filter):
@@ -291,26 +392,17 @@ if __name__ == '__main__':
 	print("Brundige's Synth!")
 	brundiges_synth = BrundigesSynth()
 
-	# nice_saw = Buzzer(brundiges_synth, 3, 55, phase=.25, amplitude=.25)
-	nice_beeper_c = SawtoothBuzzer(brundiges_synth, 4, 261.62)
-	nice_beeper_e = SawtoothBuzzer(brundiges_synth, 4, 329.62)
-	nice_beeper_g = SawtoothBuzzer(brundiges_synth, 4, 392.00)
-
-	# nice_square = Square(nice_beeper_c)
-	# nice_beeper_c.all_filters.append(nice_square)
-
-	# brundiges_synth.all_oscillators.append(nice_saw)
-	# brundiges_synth.all_oscillators.append(nice_beeper)
-	# brundiges_synth.all_oscillators.append(nice_beeper_b)
-	# nice_beeper.amplitude = 1000
-	brundiges_synth.all_oscillators.append(nice_beeper_c)
-	brundiges_synth.all_oscillators.append(nice_beeper_e)
-	brundiges_synth.all_oscillators.append(nice_beeper_g)
-	# brundiges_synth.all_oscillators.append(NoiseMaker(brundiges_synth, 3, amplitude=.01))
+	# brundiges_synth.all_oscillators.append(HarmonicBeeper(brundiges_synth, 4, 440, harmonics=2, phase=.12, amplitude=.5))
+	brundiges_synth.all_oscillators.append(Clicker(brundiges_synth, 4, 120, frequency=110, click_waves=4))
+	# desampler = LofiDesampler(4)
+	# modulus = LofiModulus(100)
+	# brundiges_synth.all_oscillators[0].all_filters.append(desampler)
+	# brundiges_synth.all_oscillators[0].all_filters.append(modulus)
+	# brundiges_synth.all_oscillators.append(InvertedBeeper(brundiges_synth, 3, 55))
 
 	brundiges_synth.play_all_oscillators()
 	brundiges_synth.combine_waves(average_waves)
 
-	# brundiges_synth.plot_oscillators(plot_oscillator_waves=False, plot_combined_wave=True)
+	brundiges_synth.plot_oscillators(plot_oscillator_waves=False, plot_combined_wave=True)
 	brundiges_synth.export_combined_wave()
 	print("Complete!")
